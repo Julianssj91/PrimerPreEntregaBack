556dc48b081c56cc85fdfe8ddec6f8b5346fc775
Julian Molina <ejmolinax91@gmail.com>
2023-06-15 18:55:15 -0300

    desafio

--- src/server.js
+++ src/server.js
@@ -3,165 +3,117 @@ const multer = require('multer');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
-const productosData = fs.readFileSync('src/data/productos.json', 'utf-8');
-const productos = JSON.parse(productosData);
+const exphbs = require('express-handlebars');
+const http = require('http');
+const io = require('socket.io')(http);
 
 const app = express();
 const PORT = 8080;
 
+// Configuración de Multer para el manejo de archivos
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    },
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
 const upload = multer({ storage });
 
+// Middleware para procesar JSON en las solicitudes
 app.use(express.json());
 
-let products = productos;
-let carts = [];
+// Configurar el motor de plantillas Handlebars
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
+// Datos de ejemplo para productos y carritos
+let products = [];
+
+// Rutas para el manejo de productos
 const productsRouter = express.Router();
 
 productsRouter.get('/', (req, res) => {
-    res.json(products);
+  res.json(products);
 });
 
 productsRouter.get('/:pid', (req, res) => {
-    const { pid } = req.params;
-    const product = products.find((p) => p.id === parseInt(pid));
+  const { pid } = req.params;
+  const product = products.find((p) => p.id === pid);
 
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404).json({ error: 'Producto no encontrado' });
-    }
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ error: 'Producto no encontrado' });
+  }
 });
 
 productsRouter.post('/', (req, res) => {
-    const { name, price, code } = req.body;
+  const { name, price, code } = req.body;
 
-    if (!name || !price || !code) {
-        res.status(400).json({ error: 'Faltan campos obligatorios' });
-        return;
-    }
+  if (!name || !price || !code) {
+    res.status(400).json({ error: 'Faltan campos obligatorios' });
+    return;
+  }
 
-    const newProduct = {
-        id: uuidv4(),
-        name,
-        price,
-        code,
-    };
+  const newProduct = {
+    id: uuidv4(),
+    name,
+    price,
+    code,
+  };
 
-    products.push(newProduct);
-    res.status(201).json(newProduct);
+  products.push(newProduct);
+
+  // Enviar la lista de productos actualizada a través de Socket.IO
+  io.emit('productAdded', products);
+
+  res.status(201).json(newProduct);
 });
 
-productsRouter.put('/:pid', (req, res) => {
-    const { pid } = req.params;
-    const { name, price, code } = req.body;
+// ... Resto de rutas para actualización, eliminación, etc.
 
-    const product = products.find((p) => p.id === parseInt(pid));
+// Ruta para subir archivos
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    res.status(400).json({ error: 'No se ha seleccionado ningún archivo' });
+    return;
+  }
 
-    if (product) {
-        product.name = name || product.name;
-        product.price = price || product.price;
-        product.code = code || product.code;
-
-        res.json(product);
-    } else {
-        res.status(404).json({ error: 'Producto no encontrado' });
-    }
+  const filePath = path.join(__dirname, 'uploads', req.file.filename);
+  res.status(200).json({ message: 'Archivo subido correctamente', filePath });
 });
 
-productsRouter.delete('/:pid', (req, res) => {
-    const { pid } = req.params;
+// Ruta para la vista "home.handlebars"
+app.get('/', (req, res) => {
+  res.render('home', { products: products });
+});
 
-    const productIndex = products.findIndex((p) => p.id === parseInt(pid));
+// Ruta para la vista "realTimeProducts.handlebars"
+app.get('/realtimeproducts', (req, res) => {
+  res.render('realTimeProducts', { products: products });
+});
+
+// Configurar Socket.IO
+io.on('connection', (socket) => {
+  console.log('Nuevo cliente conectado');
+
+  // Escuchar eventos desde el cliente
+  socket.on('deleteProduct', (productId) => {
+    const productIndex = products.findIndex((p) => p.id === productId);
 
     if (productIndex !== -1) {
-        products.splice(productIndex, 1);
-        res.sendStatus(204);
-    } else {
-        res.status(404).json({ error: 'Producto no encontrado' });
+      products.splice(productIndex, 1);
+
+      // Enviar la lista de productos actualizada a través de Socket.IO
+      io.emit('productDeleted', products);
     }
+  });
 });
 
-const cartsRouter = express.Router();
-
-cartsRouter.get('/', (req, res) => {
-    res.json(carts);
-});
-
-cartsRouter.get('/:cid', (req, res) => {
-    const { cid } = req.params;
-    const cart = carts.find((c) => c.id === cid);
-
-    if (cart) {
-        res.json(cart);
-    } else {
-        res.status(404).json({ error: 'Carrito no encontrado' });
-    }
-});
-
-cartsRouter.post('/', (req, res) => {
-    const newCart = {
-        id: uuidv4(),
-        products: [],
-    };
-
-    carts.push(newCart);
-    res.status(201).json(newCart);
-});
-
-cartsRouter.post('/:cid/products/:pid', (req, res) => {
-    const { cid, pid } = req.params;
-    const cart = carts.find((c) => c.id === cid);
-    const product = products.find((p) => p.id === parseInt(pid));
-
-    if (!cart) {
-        res.status(404).json({ error: 'Carrito no encontrado' });
-        return;
-    }
-
-    if (!product) {
-        res.status(404).json({ error: 'Producto no encontrado' });
-        return;
-    }
-
-    const existingProduct = cart.products.find((p) => p.product === pid);
-
-    if (existingProduct) {
-        existingProduct.quantity++;
-    } else {
-        cart.products.push({ product: pid, quantity: 1 });
-    }
-
-    res.json(cart.products);
-});
-
-app.use('/products', productsRouter);
-app.use('/carts', cartsRouter);
-
-app.post('/upload', upload.single('file'), (req, res) => {
-    if (!req.file) {
-        res.status(400).json({ error: 'No se ha seleccionado ningún archivo' });
-        return;
-    }
-
-    const filePath = path.join(__dirname, 'uploads', req.file.filename);
-    res.status(200).json({ message: 'Archivo subido correctamente', filePath });
-});
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Error interno del servidor' });
-});
-
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+// Iniciar el servidor
+http.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
